9a9fe69ca0fbbd63c9bd52836e849072b70b3a88
cjoanne - Oleg Vastaev <cjoanne@student.21-school.ru>
2021-04-26 16:38:28 +0300

    strchr

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -1,14 +1,6 @@
 //part1
 #include "libft.h"
 
-// static int	ft_charcmp(char c1, char c2)
-// {
-// 	if (c1 == c2)
-// 		return (1);
-// 	else
-// 		return (0);
-// }
-
 char	*ft_strchr(const char *s, int c)
 {
 	unsigned char	chr;
