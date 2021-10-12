eb8a6cc1c0270806dbea23b5b09d294cc47b251a
cjoanne - Oleg Vastaev <cjoanne@student.21-school.ru>
2021-10-12 16:05:32 +0300

    updating lib

--- srcs/ft_calloc.c
+++ srcs/ft_calloc.c
@@ -6,20 +6,22 @@
 /*   By: cjoanne <cjoanne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/22 22:00:20 by cjoanne           #+#    #+#             */
-/*   Updated: 2021/04/24 17:52:22 by cjoanne          ###   ########.fr       */
+/*   Updated: 2021/10/12 16:04:59 by cjoanne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//part1
 #include "libft.h"
 
 void	*ft_calloc(size_t count, size_t size)
 {
-	char	*str;
+	unsigned char	*str;
+	size_t			len;
 
-	str = malloc(count * size);
+	len = size * count;
+	str = malloc(len);
 	if (str == NULL)
 		return (NULL);
-	ft_bzero(str, count * size);
+	while (len--)
+		*str++ = '\0';
 	return (str);
 }
